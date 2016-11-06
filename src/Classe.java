fda27ab3409585541779933679625a6db8da18e0
NicolasDutronc <nicolas.dutronc@orange.fr>
2016-11-06 12:29:01 +0100

    nouvelle image

--- src/Classe.java
+++ src/Classe.java
@@ -7,19 +7,18 @@ import java.io.IOException;
 
 import org.jfree.graphics2d.svg.SVGGraphics2D;
 
-public class Classe {	
-	
-	
-public static void main(String[] args) throws IOException {
-	 SVGGraphics2D g2 = new SVGGraphics2D(300, 200);
-	 g2.setPaint(Color.RED);
-	 g2.draw(new Rectangle(10, 10, 280, 180));
-	 String svgElement = g2.getSVGElement();
-	 System.out.println(svgElement);
-	 String filename = "image" + System.currentTimeMillis() + ".svg";
-	 File image = new File(filename);
-	 BufferedWriter writer=new BufferedWriter(new FileWriter(image));
-	 writer.write(svgElement);
-	 writer.close();
-}	
+public class Classe {
+
+	public static void main(String[] args) throws IOException {
+		SVGGraphics2D g2 = new SVGGraphics2D(300, 200);
+		g2.setPaint(Color.RED);
+		g2.draw(new Rectangle(10, 10, 280, 180));
+		String svgElement = g2.getSVGElement();
+		System.out.println(svgElement);
+		String filename = "image" + System.currentTimeMillis() + ".svg";
+		File image = new File(filename);
+		BufferedWriter writer = new BufferedWriter(new FileWriter(image));
+		writer.write(svgElement);
+		writer.close();
+	}
 }
