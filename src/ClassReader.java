4c90de7bb9fac3c81ceb221146122ba7024a0dba
NicolasDutronc <nicolas.dutronc@orange.fr>
2016-11-13 17:05:53 +0100

    Nouvelle methode pour afficher les donnees recuperee dans la console : 'print' Creation d'un test pour une interface

---
+++ src/ClassReader.java
@@ -0,0 +1,128 @@
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+
+public class ClassReader {
+
+	/**
+	 * Static method used to retrieve data from a class
+	 * 
+	 * @param c
+	 *            the specified class
+	 * @return A list of 4 arrays with the data in this format :<br/>
+	 *         <ol>
+	 *         <li>the header which includes the type of the java object, the
+	 *         name and the package
+	 *         <li/>
+	 *         the fields of the class in parameter
+	 *         <li/>
+	 *         the constructors
+	 *         <li/>
+	 *         the methods
+	 *         <ol/>
+	 */
+	public static ArrayList<String[]> getData(Class c) {
+		// header : type + name + package
+		String[] header = new String[3];
+		if (c.isAnnotation()) {
+			header[0] = "Java annotation";
+		} else if (c.isEnum()) {
+			header[0] = "Java enumeration";
+		} else if (c.isInterface()) {
+			header[0] = "Java interface";
+		} else {
+			header[0] = "Java class";
+		}
+		header[1] = c.getName();
+		if (c.getPackage() == null) {
+			header[2] = "default";
+		} else {
+			header[2] = c.getPackage().getName();
+		}
+
+		// fields
+		String[] fields = new String[c.getDeclaredFields().length];
+		for (int i = 0; i != fields.length; i++) {
+			fields[i] = c.getDeclaredFields()[i].getName() + ": "
+					+ c.getDeclaredFields()[i].getType().getSimpleName();
+		}
+
+		// constructors
+		String[] constructors = new String[c.getDeclaredConstructors().length];
+		for (int i = 0; i != constructors.length; i++) {
+			Parameter[] param = c.getDeclaredConstructors()[i].getParameters();
+			constructors[i] = c.getDeclaredConstructors()[i].getName() + "(";
+			for (int j = 0; j < param.length - 1; j++) {
+				constructors[i] += param[j].getType().getSimpleName() + ", ";
+			}
+			if (param.length > 0) {
+				constructors[i] += param[param.length - 1].getType()
+						.getSimpleName() + ")";
+			} else {
+				constructors[i] += ")";
+			}
+		}
+
+		// methods
+		String[] methods = new String[c.getDeclaredMethods().length];
+		for (int i = 0; i != methods.length; i++) {
+			Parameter[] param = c.getDeclaredMethods()[i].getParameters();
+			methods[i] = c.getDeclaredMethods()[i].getName() + "(";
+			for (int j = 0; j < param.length - 1; j++) {
+				methods[i] += param[j].getType().getSimpleName() + ", ";
+			}
+			if (param.length > 0) {
+				methods[i] += param[param.length - 1].getType().getSimpleName()
+						+ ")";
+			} else {
+				methods[i] += ")";
+			}
+			methods[i] += " : "
+					+ c.getDeclaredMethods()[i].getReturnType().getSimpleName();
+		}
+
+		ArrayList<String[]> res = new ArrayList<String[]>();
+		res.add(header);
+		res.add(fields);
+		res.add(constructors);
+		res.add(methods);
+
+		return res;
+	}
+
+	/**
+	 * Static method used to print the data retrieved from the class c with the
+	 * method 'getData'
+	 * 
+	 * @param c
+	 *            class
+	 */
+	public static void print(Class c) {
+		ArrayList<String[]> s = getData(c);
+
+		System.out.println("type : " + s.get(0)[0]);
+		System.out.println("name : " + s.get(0)[1]);
+		System.out.println("package : " + s.get(0)[2]);
+		System.out.println();
+		System.out.println("fields : ");
+		for (int i = 0; i != s.get(1).length; i++) {
+			System.out.println(s.get(1)[i]);
+		}
+		System.out.println();
+		System.out.println("constructors : ");
+		for (int i = 0; i != s.get(2).length; i++) {
+			System.out.println(s.get(2)[i]);
+		}
+		System.out.println();
+		System.out.println("methods : ");
+		for (int i = 0; i != s.get(3).length; i++) {
+			System.out.println(s.get(3)[i]);
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		print(TestInterface.class);
+		print(TestClassReader.class);
+	}
+
+}
---
+++ src/TestClassReader.java
@@ -0,0 +1,43 @@
+public class TestClassReader {
+
+	private double attr1;
+	private int attr2;
+	private String attr3;
+
+	public TestClassReader(double attr1, int attr2, String attr3) {
+		this.attr1 = attr1;
+		this.attr2 = attr2;
+		this.attr3 = attr3;
+	}
+
+	public TestClassReader() {
+		this.attr1 = 0.0;
+		this.attr2 = 0;
+		this.attr3 = "";
+	}
+
+	public double getAttr1() {
+		return attr1;
+	}
+
+	public void setAttr1(double attr1) {
+		this.attr1 = attr1;
+	}
+
+	public int getAttr2() {
+		return attr2;
+	}
+
+	public void setAttr2(int attr2) {
+		this.attr2 = attr2;
+	}
+
+	public String getAttr3() {
+		return attr3;
+	}
+
+	public void setAttr3(String attr3) {
+		this.attr3 = attr3;
+	}
+
+}
